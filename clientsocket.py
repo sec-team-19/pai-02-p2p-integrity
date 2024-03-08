14d5b41a9ee5abea3f5c929497b6e5268e433276
adrf <adrromflo@alum.us.es>
2024-03-08 14:05:00 +0100

    feat: initial version

--- clientsocket.py
+++ clientsocket.py
@@ -1,13 +1,21 @@
 # clientsocket.py
 
 import socket
+import secrets
+import hmac
 
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 3030  # The port used by the server
+KEY = b"IN$3GU$/s3k-t34m-n1n3t3en"
+SEP = b"|||"
+
+def generate_nonce():
+    return secrets.token_bytes(32)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
+    nonce = generate_nonce()
+    message = b"Hello, world"
+    hash = hmac.new(KEY, nonce + SEP + message, "sha3_256").digest()
+    s.sendall(nonce + SEP + message + SEP + hash)
 
-print(f"Received {data.decode('utf-8')}")
--- serversocket.py
+++ serversocket.py
@@ -1,9 +1,35 @@
 # serversocket.py
 
 import socket
+import sqlite3
+import hmac
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 3030  # Port to listen on (non-privileged ports are > 1023)
+KEY = b"IN$3GU$/s3k-t34m-n1n3t3en"
+DB = "pai-02-p2p-integrity.db"
+SEP = b"|||"
+
+# create a database connection
+
+conn = sqlite3.connect(DB)
+c = conn.cursor()
+c.execute(
+    """
+    CREATE TABLE IF NOT EXISTS messages (
+        id TEXT PRIMARY KEY NOT NULL,
+        message TEXT NOT NULL
+    )
+    """
+)
+
+def insert_message(id, message):
+    c.execute("INSERT INTO messages (id, message) VALUES (?, ?)", (id, message))
+    conn.commit()
+
+def check_id_exists(id):
+    c.execute("SELECT * FROM messages WHERE id=?", (id,))
+    return c.fetchone() is not None
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -13,7 +39,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print(f"Connected by {addr}")
         while True:
             data = conn.recv(1024)
-            print(data.decode("utf-8"))
+            data_list = data.split(SEP)
+            if len(data_list) != 3:
+                print("Invalid data received")
+                break
+            nonce = data_list[0]
+            message = data_list[1]
+            hash_rec = data_list[2]
+            hash = hmac.new(KEY, nonce + SEP + message, "sha3_256").digest()
+            if hash != hash_rec:
+                print("Hashes do not match")
+                break
             if not data:
                 break
-            conn.sendall(data)
