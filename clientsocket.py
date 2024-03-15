75181861f042350919ccc99816540d5bed6784af
adrf <adrromflo@alum.us.es>
2024-03-15 10:43:09 +0100

    fix: testing

--- clientsocket.py
+++ clientsocket.py
@@ -12,17 +12,22 @@ SEP = b"|||"
 
 
 def generate_nonce(sim_same_nonce=False):
+    # testing purposes only delete this in production
     if sim_same_nonce:
         return b"0" * 32
+    # -----------------------------------------------
+
     timestamp = int.to_bytes(int(time.time()), 8, "big")
     random = secrets.token_bytes(24)
     return timestamp + random
 
 
+# testing purposes only delete this in production
 sim_same_nonce = input("Simulate same nonce? (y/n): ").strip().lower() == "y"
 if sim_same_nonce:
     print("Send another message with same nonce to simulate a reply attack.")
 sim_mitm = input("Simulate MITM attack? (y/n): ").strip().lower() == "y"
+# -----------------------------------------------
 acc_origin = input("Account origin: ").strip()
 acc_dest = input("Account destination: ").strip()
 amount = input("Amount: ").strip()
@@ -42,6 +47,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     nonce = generate_nonce(sim_same_nonce)
     hash = hmac.new(KEY, nonce + SEP + message, "sha3_256").digest()
+    # testing purposes only delete this in production
     if sim_mitm:
         message = b"Hello, i'm a MITM!"
+    # -----------------------------------------------
     s.sendall(nonce + SEP + message + SEP + hash)
